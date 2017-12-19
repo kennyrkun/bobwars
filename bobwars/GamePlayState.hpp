cc66e93392dd1760b25d08d33667823be17f60e8
Kenny McCormick <keennymccormic+github@gmail.com>
2017-12-19 16:37:48 -0600

    update version number
    0.7.3 to 0.8.0

--- bobwars/GamePlayState.hpp
+++ bobwars/GamePlayState.hpp
@@ -25,7 +25,7 @@ public:
 	void Update();
 	void Draw();
 
-	std::string gameVersion = "0.7.3";
+	std::string gameVersion = "0.8.0";
 //	sf::RenderWindow *app->window;
 	sf::Time timePerFrame; // 60 frames per second
 	sf::Time timeSinceLastUpdate;
