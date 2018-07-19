daff966ab4ce23d82c69cc73552f1d3c17439dcd
Kenny McCormick <keennymccormic+github@gmail.com>
2018-07-18 19:28:20 -0500

    update main and .gitignore

--- bobwars/main.cpp
+++ bobwars/main.cpp
@@ -1,70 +1,9 @@
 #include "AppEngine.hpp"
 #include "MainMenuState.hpp"
-
 #include "Util/Logger.hpp"
+#include "Util/DiscordRPC.hpp"
 
-#include <iostream>
-
-#include <SFUI/SFUI.hpp>
-
-#include <DISCORD/discord_rpc.h>
-
-class DiscordRPC
-{
-public:
-	static void handleDiscordReady(const DiscordUser* connectedUser)
-	{
-		logger::INFO("[DISCORD] Connected!");
-		logger::INFO("[DISCORD] User: " + std::string(connectedUser->username) + std::string(connectedUser->discriminator) + " (" + std::string(connectedUser->userId) + ")");
-	}
-
-	static void handleDiscordError(int errorCode, const char* message)
-	{
-		logger::ERROR("[DISCORD] Disconnected: " + std::string(message) + " (" + std::to_string(errorCode) + ")");
-	}
-
-	static void handleDiscordDisconnected(int errorCode, const char* message)
-	{
-		logger::ERROR("[DISCORD] " + std::string(message) + " (" + std::to_string(errorCode) + ")");
-	}
-
-	void updateDiscordPresence()
-	{
-		char buffer[256];
-
-		DiscordRichPresence discordPresence;
-		memset(&discordPresence, 0, sizeof(discordPresence));
-		discordPresence.state = "in Main Menu";
-		//	sprintf(buffer, "Frustration level: %d", FrustrationLevel);
-		//	discordPresence.details = "probably beating off";
-		//	discordPresence.endTimestamp = time(0) + 5 * 60;
-		discordPresence.largeImageKey = "large-bob";
-		discordPresence.smallImageKey = "side-google";
-		//	discordPresence.partyId = "party1234";
-		//	discordPresence.partySize = 1;
-		//	discordPresence.partyMax = 6;
-		//	discordPresence.instance = 0;
-
-		Discord_UpdatePresence(&discordPresence);
-	}
-
-	void InitDiscord()
-	{
-		DiscordEventHandlers handlers;
-		memset(&handlers, 0, sizeof(handlers));
-
-		handlers.ready = &this->handleDiscordReady;
-		handlers.errored = &this->handleDiscordError;
-		handlers.disconnected = &this->handleDiscordDisconnected;
-		//	handlers.joinGame = handleDiscordJoinGame;
-		//	handlers.spectateGame = handleDiscordSpectateGame;
-		//	handlers.joinRequest = handleDiscordJoinRequest;
-
-		// Discord_Initialize(const char* applicationId, DiscordEventHandlers* handlers, int autoRegister, const char* optionalSteamId)
-		Discord_Initialize("468957546359685121", &handlers, false, nullptr);
-	}
-};
-
+#include <SFUI/Theme.hpp>
 
 int main(int argc, char *argv[])
 {
@@ -75,8 +14,7 @@ int main(int argc, char *argv[])
 
 	AppSettings options;
 
-	int fpsLimit(60); // default 60
-
+	/*
 	for (int i = 0; i < argc; i++)
 	{
 		if (std::string(argv[i]) == "-fullscreen")
@@ -85,12 +23,13 @@ int main(int argc, char *argv[])
 		if (std::string(argv[i]) == "-vsync")
 			options.vsync = true;
 
-//		if (std::string(argv[i]) == "-fps")
-//		{
-			// next arg should be an int (the fps)
-//			fpsLimit == std::stoi(argv[i + 1]);
-//		}
+		if (std::string(argv[i]) == "-maxfps")
+		{
+			// next arg should be an int (the maxfps)
+			options.maxfps = std::stoi(argv[i += 1]);
+		}
 	}
+	*/
 
 	logger::INFO("Preparing SFUI");
 	SFUI::Theme::loadFont("bobwars/resource/interface/tahoma.ttf");
@@ -106,8 +45,10 @@ int main(int argc, char *argv[])
 	SFUI::Theme::PADDING = 2.f;
 
 	DiscordRPC dRPC;
-	dRPC.InitDiscord();
-	dRPC.updateDiscordPresence();
+	dRPC.Init();
+
+	dRPC.setState("at Main Menu");
+	dRPC.updatePresence();
 
 	AppEngine app;
 	app.Init("bobwars", options);
@@ -125,11 +66,10 @@ int main(int argc, char *argv[])
 		app.Update();
 		app.Draw();
 
-//		Discord_UpdateConnection();
-		Discord_RunCallbacks();
+		dRPC.update();
 	}
 
-	Discord_Shutdown();
+	dRPC.Shutdown();
 
 	logger::INFO("Exiting...");
 	return EXIT_SUCCESS;
