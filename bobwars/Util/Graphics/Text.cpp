9afa1b9315cca7a42394aebb75f0bb9efb0cb5be
Kenny McCormick <keennymccormic+github@gmail.com>
2018-07-19 10:03:01 -0500

    added Text drawing function that requires no reference to text

--- bobwars/Util/Graphics/Text.cpp
+++ bobwars/Util/Graphics/Text.cpp
@@ -51,5 +51,18 @@ namespace text
 
 		window.draw(text);
 	}
+
+	void draw(sf::RenderWindow &window, const std::string string, const sf::Vector2f &pos)
+	{
+		sf::Text text;
+		sf::Font font;
+		font.loadFromFile("C:/Windows/Fonts/Arial.ttf");
+		text.setFont(font);
+		text.setCharacterSize(12);
+		text.setPosition(pos);
+		text.setString(string);
+
+		window.draw(text);
+	}
 }
 }
--- bobwars/Util/Graphics/Text.hpp
+++ bobwars/Util/Graphics/Text.hpp
@@ -7,10 +7,6 @@ namespace util {
 
 namespace text
 {
-	// TODO: we might use a different method to render text.
-	// perhaps create a string manager type class, and use only one piece of text to render everything with line breaks
-	// this could yeild a generous performance improvement.
-
 	void draw(sf::RenderWindow &window, sf::Text &text, const std::string string, const sf::Vector2f &pos, const int characterSize, const sf::Color &c);
 
 	void draw(sf::RenderWindow &window, sf::Text &text, const std::string string, const sf::Vector2f &pos, const int characterSize);
@@ -18,6 +14,8 @@ namespace text
 	void draw(sf::RenderWindow &window, sf::Text &text, const std::string string, const sf::Vector2f &pos, const sf::Vector2f& scale);
 
 	void draw(sf::RenderWindow &window, sf::Text &text, const std::string string, const sf::Vector2f &pos);
+
+	void draw(sf::RenderWindow &window, const std::string string, const sf::Vector2f &pos);
 }
 
 }
