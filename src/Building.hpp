5bb40e88bc7baeed51547b6806c3caae9919bdb6
kennyrkun <kennyrmccormick+github@gmail.com>
2020-04-20 14:17:04 -0500

    show garrisonpoint for building

--- src/Building.hpp
+++ src/Building.hpp
@@ -3,25 +3,39 @@
 
 #include "BaseEntity.hpp"
 
+#include <SFML/Graphics/CircleShape.hpp>
+
 class Building : public BaseEntity
 {
 public:
-	Building(const int entityID, AppEngine* app = nullptr) : BaseEntity(entityID, true, true, false, false, false, EntityType::BuildingEntity, app) {}
+	Building(const int entityID, AppEngine* app = nullptr) : BaseEntity(entityID, true, true, false, false, false, EntityType::BuildingEntity, app)
+	{
+		garrisonPoint.setRadius(2);
+		garrisonPoint.setOrigin(sf::Vector2f(1, 1));
+	}
 
 	inline void setGarrisonPoint(const sf::Vector2f& point)
 	{
-		garrisonPoint = point;
+		garrisonPoint.setPosition(point);
 	}
 
 	inline const sf::Vector2f& getGarrisonPoint() const
 	{
-		return garrisonPoint;
+		return garrisonPoint.getPosition();
 	}
 
-	bool hasGarrisonPoint;
+	bool hasGarrisonPoint = false;
+
+	inline void draw(sf::RenderTarget& target, sf::RenderStates states) const override
+	{
+		BaseEntity::draw(target, states);
+
+		if (isSelected)
+			target.draw(garrisonPoint);
+	}
 
 private:
-	sf::Vector2f garrisonPoint;
+	sf::CircleShape garrisonPoint;
 };
 
 #endif // !BUILDING_HPP
